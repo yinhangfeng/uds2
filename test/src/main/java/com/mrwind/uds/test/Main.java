e977e65be4d4046f3896fef280206997cad7243e
yinhf <darkwander.y@gmail.com>
2019-05-24 21:43:54 +0800

    test

--- test/src/main/java/com/mrwind/uds/test/Main.java
+++ test/src/main/java/com/mrwind/uds/test/Main.java
@@ -1,8 +1,6 @@
 package com.mrwind.uds.test;
 
-import io.jenetics.BitChromosome;
-import io.jenetics.BitGene;
-import io.jenetics.Genotype;
+import io.jenetics.*;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionResult;
 import io.jenetics.util.Factory;
@@ -16,8 +14,7 @@ public class Main {
                 .bitCount();
     }
 
-    public static void main(String[] args) throws Exception {
-
+    static void test0() {
         // 1.) Define the genotype (factory) suitable
         //     for the problem.
         Factory<Genotype<BitGene>> gtf =
@@ -35,5 +32,54 @@ public class Main {
                 .collect(EvolutionResult.toBestGenotype());
 
         System.out.println("Hello World:\n" + result);
+    }
+
+    static Double test1Fitness(Genotype<IntegerGene> gt) {
+        IntegerChromosome chromosome = gt.getChromosome().as(IntegerChromosome.class);
+
+        int length = chromosome.length();
+        double sum = 0;
+        for (int i = 0; i < length; ++i) {
+            sum += chromosome.getGene(i).intValue();
+        }
+        double avg = sum / length;
+        double a = 0;
+        for (int i = 0; i < length; ++i) {
+            a += Math.pow(chromosome.getGene(i).intValue() - avg, 2);
+        }
+        double v = a / length;
+
+        System.out.println("fitness thread:" + Thread.currentThread().getName() + " sum: " + sum + " avg: " + avg + " v: " + v);
+
+        // 目标是使总和最大 也就是最好所有基因都为 9999
+        // 如果只是求和的话 在基因数量比较多时 最终结果没比随机好多少
+
+//        try {
+//            Thread.sleep(20);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return sum;
+    }
+
+    static void test1() {
+        Factory<Genotype<IntegerGene>> gtf = Genotype.of(IntegerChromosome.of(0, 9999, 2000));
+
+        // 对于低代价的 fitness 函数 使用多线程反而会减速
+        Engine<IntegerGene, Double> engine = Engine.builder(Main::test1Fitness, gtf)
+                .populationSize(1000)
+                .offspringFraction(0.95)
+                .executor(Runnable::run)
+                .build();
+
+        Genotype<IntegerGene> result = engine.stream().limit(100).collect(EvolutionResult.toBestGenotype());
+
+        System.out.println("result:\n" + result);
+        test1Fitness(result);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        test1();
     }
 }
